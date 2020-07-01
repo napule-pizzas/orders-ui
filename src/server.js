f7008d380b8529ef46aad24e37b6c9d60a75aa94
Miguel Senosiain <msenosiain@gmail.com>
2020-07-01 15:56:19 -0300

    Update Heroku config

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-
 const app = express();
 
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+app.use(static(__dirname + '/dist'));
 
-app.use(express.static('./dist/napule-orders-ui'));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/napule-orders-ui/'}),
-// );
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/index.html');
+});
 
 app.listen(process.env.PORT || 8080);
