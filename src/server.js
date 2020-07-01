e7e75a43273f483405008f29ab530905dbcddda8
Miguel Senosiain <msenosiain@gmail.com>
2020-07-01 15:37:43 -0300

    Update heroku deployment configuration

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 app.use(express.static('./dist/napule-orders-ui'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/napule-orders-ui/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/napule-orders-ui/'}),
+// );
 
 app.listen(process.env.PORT || 8080);
