460ef487928dad412e73c8b140d118eeb4e356cb
Miguel Senosiain <msenosiain@gmail.com>
2020-07-06 23:17:31 -0300

    Add MatErrorMessageDirective (component)

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -4,12 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { QuantitySelectorComponent } from './components/quantity-selector/quantity-selector.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatErrorMessagesDirective } from './directives/mat-error-messages.directive';
 
 const AngularMaterialModules = [MatButtonModule, MatIconModule];
 
 @NgModule({
   imports: [CommonModule, FlexLayoutModule, ...AngularMaterialModules],
-  declarations: [QuantitySelectorComponent],
-  exports: [QuantitySelectorComponent]
+  declarations: [QuantitySelectorComponent, MatErrorMessagesDirective],
+  exports: [QuantitySelectorComponent, MatErrorMessagesDirective]
 })
 export class CoreModule {}
---
+++ src/app/@core/directives/mat-error-messages.directive.ts
@@ -0,0 +1,58 @@
+import { Component, AfterViewInit, Injector, OnInit } from '@angular/core';
+import { MatFormFieldControl, MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { ValidationErrors } from '@angular/forms';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: '[matErrorMessages]',
+  template: '{{ error }}'
+})
+// tslint:disable-next-line: component-class-suffix
+export class MatErrorMessagesDirective implements AfterViewInit {
+  error: string;
+  inputRef: MatFormFieldControl<MatInput>;
+
+  constructor(private injector: Injector) {}
+  // Setup all initial tooling
+
+  ngAfterViewInit() {
+    const container = this.injector.get(MatFormField);
+    this.inputRef = container._control;
+
+    // sub to the control's status stream
+    this.inputRef.ngControl.control.statusChanges.subscribe(state => {
+      if (state === 'INVALID') {
+        const controlErrors: ValidationErrors = this.inputRef.ngControl.control.errors;
+        let firstValidationError: string;
+        if (Array.isArray(controlErrors)) {
+          firstValidationError = Object.keys(controlErrors[0])[0];
+        } else {
+          firstValidationError = Object.keys(controlErrors)[0];
+        }
+
+        switch (firstValidationError) {
+          case 'required':
+            this.error = 'Esto es necesario';
+            break;
+          case 'phone':
+          case 'email':
+            this.error = 'Esto no está bien';
+            break;
+          case 'minlength':
+            this.error = `Deben ser al menos ${controlErrors[firstValidationError].requiredLength} caracteres`;
+            break;
+          case 'maxlength':
+            this.error = `Deben ser ${controlErrors[firstValidationError].requiredLength} caracteres máximo`;
+            break;
+          case 'passwordMissmatch':
+            this.error = 'Contraseña y confirmación no conciden';
+            break;
+          default:
+            console.log(controlErrors);
+            break;
+        }
+      }
+    });
+  }
+}
